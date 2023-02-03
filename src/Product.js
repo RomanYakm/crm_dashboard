dbe17f38d74ffcf7795a6cf326adfdf4a49f2fd4
Roman Yakymiuk <romeronice93@gmail.com>
2023-02-03 13:41:44 +0200

    Working on pagination

--- src/Product.js
+++ src/Product.js
@@ -1,4 +1,34 @@
+import { useState } from "react";
+
 const Product = () => {
+  const [data, setData] = useState(null);
+  const [pageCounter, setPageCounter] = useState(0);
+  let pages = 0;
+
+  const getData = () => {
+    fetch("./CustomersList.json")
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        setData(myJson);
+      });
+  };
+
+  getData();
+
+  if (data) {
+    pages = Math.ceil(data.length / 8);
+  }
+
+  const nextPage = () => {
+    setPageCounter(pageCounter + 8);
+  };
+
+  const prevPage = () => {
+    setPageCounter(pageCounter - 8);
+  };
+  
   return (
     <section className="Product">
       <h1 className="Product__title title">Hello Evano 👋🏼,</h1>
@@ -12,81 +42,36 @@ const Product = () => {
         </div>
         <div className="customers__list__table">
           <table className="customers__list__table__table">
-            <tr className="customers__list__table__top">
-              <th className="customers__list__table__heading">Customer Name</th>
-              <th className="customers__list__table__heading">Company</th>
-              <th className="customers__list__table__heading">Phone Number</th>
-              <th className="customers__list__table__heading">Email</th>
-              <th className="customers__list__table__heading">Country</th>
-              <th className="customers__list__table__heading__status">Status</th>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Jane Cooper</td>
-              <td className="customers__list__table__element">Microsoft</td>
-              <td className="customers__list__table__element">(225) 555-0118</td>
-              <td className="customers__list__table__element">jane@microsoft.com</td>
-              <td className="customers__list__table__element">United States</td>
-              <td className="customers__list__table__element__status__active">Active</td>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Floyd Miles</td>
-              <td className="customers__list__table__element">Yahoo</td>
-              <td className="customers__list__table__element">(205) 555-0100</td>
-              <td className="customers__list__table__element">floyd@yahoo.com</td>
-              <td className="customers__list__table__element">Kiribati</td>
-              <td className="customers__list__table__element__status__inactive">Inactive</td>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Floyd Miles</td>
-              <td className="customers__list__table__element">Yahoo</td>
-              <td className="customers__list__table__element">(205) 555-0100</td>
-              <td className="customers__list__table__element">floyd@yahoo.com</td>
-              <td className="customers__list__table__element">Kiribati</td>
-              <td className="customers__list__table__element__status__inactive">Inactive</td>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Floyd Miles</td>
-              <td className="customers__list__table__element">Yahoo</td>
-              <td className="customers__list__table__element">(205) 555-0100</td>
-              <td className="customers__list__table__element">floyd@yahoo.com</td>
-              <td className="customers__list__table__element">Kiribati</td>
-              <td className="customers__list__table__element__status__active">Active</td>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Floyd Miles</td>
-              <td className="customers__list__table__element">Yahoo</td>
-              <td className="customers__list__table__element">(205) 555-0100</td>
-              <td className="customers__list__table__element">floyd@yahoo.com</td>
-              <td className="customers__list__table__element">Kiribati</td>
-              <td className="customers__list__table__element__status__active">Active</td>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Floyd Miles</td>
-              <td className="customers__list__table__element">Yahoo</td>
-              <td className="customers__list__table__element">(205) 555-0100</td>
-              <td className="customers__list__table__element">floyd@yahoo.com</td>
-              <td className="customers__list__table__element">Kiribati</td>
-              <td className="customers__list__table__element__status__active">Active</td>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Jacob Jones</td>
-              <td className="customers__list__table__element">Yahoo</td>
-              <td className="customers__list__table__element">(208) 555-0112</td>
-              <td className="customers__list__table__element">jacob@yahoo.com</td>
-              <td className="customers__list__table__element">Brazil</td>
-              <td className="customers__list__table__element__status__active">Active</td>
-            </tr>
-            <tr className="customers__list__table__element__container">
-              <td className="customers__list__table__element">Kristin Watson</td>
-              <td className="customers__list__table__element">Facebook</td>
-              <td className="customers__list__table__element">(704) 555-0127</td>
-              <td className="customers__list__table__element">kristin@facebook.com</td>
-              <td className="customers__list__table__element">Åland Islands</td>
-              <td className="customers__list__table__element__status__inactive">Inactive</td>
-            </tr>
+            <tbody>
+              <tr className="customers__list__table__top">
+                <th className="customers__list__table__heading">Customer Name</th>
+                <th className="customers__list__table__heading">Company</th>
+                <th className="customers__list__table__heading">Phone Number</th>
+                <th className="customers__list__table__heading">Email</th>
+                <th className="customers__list__table__heading">Country</th>
+                <th className="customers__list__table__heading__status">Status</th>
+              </tr>
+              {data && data.slice(pageCounter, pageCounter + 8).map((item) => 
+                <tr className="customers__list__table__element__container" key={item.id}>
+                <td className="customers__list__table__element">{item.customerName}</td>
+                <td className="customers__list__table__element">{item.company}</td>
+                <td className="customers__list__table__element">{item.phoneNumber}</td>
+                <td className="customers__list__table__element">{item.email}</td>
+                <td className="customers__list__table__element">{item.country}</td>
+                {item.status === "Active" &&
+                <td className="customers__list__table__element__status__active">{item.status}</td>}
+                {item.status === "Inactive" &&
+                <td className="customers__list__table__element__status__inactive">{item.status}</td>}
+              </tr>
+              )}
+            </tbody>
           </table>
-          <p className="customers__list__numbers__data">Showing data 1 to 8 of  256K entries</p>
-          <div></div>
+          <p className="customers__list__numbers__data">Showing data 1 to 8 of {data && data.length} entries</p>
+          <div>
+            <button onClick={prevPage}>Prev Page</button>
+            {pages}
+            <button onClick={nextPage}>Next Page</button>
+          </div>
         </div>
       </div>
     </section>
